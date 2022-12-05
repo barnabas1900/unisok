6bdf72117aecbd4e27f68b882be6de47d5271c0d
Barnabas <learningmytube@gmail.com>
2022-12-05 20:36:34 +0100

    ret

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,19 +6,21 @@ import caret from "../icons/caret.svg";
 const Hero = () => {
   return (
     <>
-      <div className="hero-image">
-        <img src={hero} alt="" />
-      </div>
-      <div className="image-content">
-        <h1>WELCOME TO USMANU DANFODIYO UNIVERSITY SOKOTO.</h1>
-        <p className="medium">The most peaceful university in Nigeria</p>
-      </div>
-      <a href="">
-        <div className="learn-more-div">
-          <p className="bolder-white">Learn More</p>
-          <img src={caret} alt="" />
+      <div className="hero-container">
+        <div className="hero-image">
+          <img src={hero} alt="" />
         </div>
-      </a>
+        <div className="image-content">
+          <h1>WELCOME TO USMANU DANFODIYO UNIVERSITY SOKOTO.</h1>
+          <p className="medium">The most peaceful university in Nigeria</p>
+        </div>
+        <a href="">
+          <div className="learn-more-div">
+            <p className="bolder-white">Learn More</p>
+            <img src={caret} alt="" />
+          </div>
+        </a>
+      </div>
     </>
   );
 };
